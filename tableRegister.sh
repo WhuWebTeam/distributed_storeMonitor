acb93c97d091116fc52b441bbef999c06a5ff25f
qwasxj <1942750262@qq.com>
2017-12-19 16:36:08 -0500

    temp

--- tableRegister.sh
+++ tableRegister.sh
@@ -23,5 +23,38 @@ ALTER TABLE public.${5}_counters
   OWNER TO wesine_lpssystem;
 
 
+CREATE TABLE public.${5}_eventsList
+(
+    sysKey varchar(50) primary key,
+    shopId varchar(50),
+    shopName varchar(50),
+    cashierId varchar(50),
+    cashierName varchar(50),
+    counterId varchar(50),
+    counterType varchar(50),
+    productId varchar(50),
+    productName varchar(50),
+    transId varchar(50),
+    ts bigint,
+    videoUrl varchar(50),
+    videoStartTime bigint,
+    videoEndTime bigint,
+    pic1Url varchar(50),
+    pic2Url varchar(50),
+    pic3Url varchar(50),
+    pic4Url varchar(50),
+    createAt bigint,
+    status bigint,
+    editResult varchar(50),
+    comments varchar(50)
+)
+WITH (
+  OIDS = FALSE
+);
+ALTER TABLE public.${5}_eventsList
+  OWNER TO wesine_lpssystem;
+
+
+
 
 "
--- tableRetrieve.sh
+++ tableRetrieve.sh
@@ -3,5 +3,6 @@
 psql -U ${1} -d ${2} -h ${3} -p ${4} --command "
 
 drop table ${5}_counters;
+drop table ${5}_eventsList;
 
 "
